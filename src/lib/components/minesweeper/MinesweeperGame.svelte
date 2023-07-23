c42ea1e6fe0ba78936fbe47068bef4d296239c0c
HSteffensen <henry@steffensenfamily.com>
2023-07-23 11:47:28 +0200

    Initial setup

---
+++ src/lib/components/minesweeper/MinesweeperGame.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="grid grid-cols-1">
+    <div class="outline outline-1 outline-blue-500">Top box</div>
+    <div class="outline outline-1 outline-green-500">Game grid goes here</div>
+</div>
